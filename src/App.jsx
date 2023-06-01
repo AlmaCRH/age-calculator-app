3303ea5aec65962c614540d21a66f85b4d212351
alma <alma_cruz_hernandez@outlook.com>
2023-06-01 20:19:05 +0000

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import './App.css'
 function App() {
 
   return (
-   <div>AA</div>
+   <div className='bg-gray-500 h-full flex justify-center items-center'>
+    <div className='bg-white flex justify-center items-center h-full w-full'>
+      <div className='flex flex-col'>
+      <label>DAY</label>
+      <input type="number" className='border-2 rounded-lg h-16 w-40 '/>
+        </div>
+    </div>
+   </div>
   )
 }
 
